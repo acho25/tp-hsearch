924077a4806b8c50f380016de31564dcce6082a1
acho25 <you@example.com>
2022-03-07 17:00:38 +0100

    init partie 2

--- src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/search/M1DidAnalyzer.java
+++ src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/search/M1DidAnalyzer.java
@@ -1,26 +1,24 @@
 package fr.univtln.dapm.bda.hsearch_elasticsearch.search;
 
 
-import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
-import org.apache.lucene.analysis.core.StopFilterFactory;
-import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
-import org.apache.lucene.analysis.ngram.EdgeNGramFilterFactory;
-import org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory;
-import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
-import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
-import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
 
-public class M1DidAnalyzer implements LuceneAnalysisConfigurer
-{
 
+public class M1DidAnalyzer implements ElasticsearchAnalysisConfigurer {
+	/**
+	 * Analyseur utilisant Opensearch
+	 * Si Opensearch à été lancé avec docker tous les livres indexés se trouverons dans le repertoire opensearch-data (dans docker-opensearch)
+	 * Les données seront manipulable avec opensearch et opensearch-dashboard (voir README partie 2 pour preuve)
+	 *
+	 * @param context la configuration de l'analyseur
+	 */
 	@Override
-	public void configure(LuceneAnalysisConfigurationContext context)
-	{
-		context.analyzer("m1_did_analyzer").custom().tokenizer(StandardTokenizerFactory.class)
-				.tokenFilter(LowerCaseFilterFactory.class).tokenFilter(StopFilterFactory.class)
-				.tokenFilter(ASCIIFoldingFilterFactory.class).tokenFilter(SnowballPorterFilterFactory.class)
-				.param("language", "English").tokenFilter(EdgeNGramFilterFactory.class).param("minGramSize", "3")
-				.param("maxGramSize", "7");
-	}
+	public void configure(ElasticsearchAnalysisConfigurationContext context) {
+		context.analyzer("m1_did_analyzer").custom()
+				.tokenizer("standard")
+				.tokenFilters("lowercase", "snowball_english", "asciifolding");
 
+		context.tokenFilter("snowball_english").type("snowball").param("language", "English");
+	}
 }
