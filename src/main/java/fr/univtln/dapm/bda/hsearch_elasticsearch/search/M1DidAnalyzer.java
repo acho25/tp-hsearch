2817363cd3829fd8813a27123b1ef100e973a3e6
acho25 <you@example.com>
2022-03-07 17:24:22 +0100

    fin partie 2

--- src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/search/M1DidAnalyzer.java
+++ src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/search/M1DidAnalyzer.java
@@ -6,19 +6,26 @@ import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysis
 
 
 public class M1DidAnalyzer implements ElasticsearchAnalysisConfigurer {
-	/**
-	 * Analyseur utilisant Opensearch
-	 * Si Opensearch à été lancé avec docker tous les livres indexés se trouverons dans le repertoire opensearch-data (dans docker-opensearch)
-	 * Les données seront manipulable avec opensearch et opensearch-dashboard (voir README partie 2 pour preuve)
-	 *
-	 * @param context la configuration de l'analyseur
-	 */
+
 	@Override
 	public void configure(ElasticsearchAnalysisConfigurationContext context) {
-		context.analyzer("m1_did_analyzer").custom()
-				.tokenizer("standard")
-				.tokenFilters("lowercase", "snowball_english", "asciifolding");
+		context.analyzer( "m1_did_analyzer" ).custom()
+				.tokenizer( "standard" )
+				.tokenFilters( "lowercase", "snowball_english", "asciifolding" );
 
-		context.tokenFilter("snowball_english").type("snowball").param("language", "English");
+		context.tokenFilter( "snowball_english" )
+				.type( "snowball" )
+				.param( "language", "English" )
+				.param("minGramSize", "3")
+				.param("maxGramSize", "7");
+
+		context.tokenFilter( "snowball_french" )
+				.type( "snowball" )
+				.param( "language", "French" )
+				.param("minGramSize", "3")
+				.param("maxGramSize", "7");;
+
+
 	}
+
 }
